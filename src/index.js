d09091bdc07d03f57b2a9bbc4a69f288cb106a0b
Eduard Likwong <eduard.likwong@emma-sleep.com>
2024-04-09 10:09:30 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import Fastify from "fastify";
+import Redis from "@fastify/redis";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const fastifyPluginRegistration = async (fastify) => {
+  try {
+    await fastify.register(Redis, {
+      host: "localhost",
+      username: "test",
+      password: "123",
+    });
+  } catch (err) {
+    // Handle error
+    console.error(err);
+  }
+
+  fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+  });
+};
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastifyPluginRegistration(fastify);
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
